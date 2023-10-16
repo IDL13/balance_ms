896f7aea9be7594836ccda591a0a7213eb83d53a
IDL13 <leonov.ilya.02@gmail.com>
2023-10-16 23:12:51 +0300

    docker

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,27 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/IDL13/balance_ms/internal/handlers"
 	"github.com/IDL13/balance_ms/pkg/api"
 	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
 	s := grpc.NewServer()
 	srv := &handlers.GRPCServer{}
 	api.RegisterBalanceMsServer(s, srv)
+
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error from listen server: %e", err)
+		os.Exit(1)
+	}
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatal(err)
+	}
 }
