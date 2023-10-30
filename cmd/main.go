c4a34b54d18dcf88d23b3d69efa63c4f5488d2d0
IDL13 <leonov.ilya.02@gmail.com>
2023-10-30 22:39:36 +0300

    graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/IDL13/balance_ms/gql/graph"
 	"github.com/IDL13/balance_ms/internal/CSV"
 	"github.com/IDL13/balance_ms/internal/handlers"
 	"github.com/IDL13/balance_ms/pkg/api"
 	"google.golang.org/grpc"
-	"log"
-	"net"
-	"os"
 )
 
-func main() {
+func RunGrpc(stopGrpc chan bool) {
+	go func() {
+		<-stopGrpc
+		os.Exit(1)
+	}()
+
 	s := grpc.NewServer()
 	//srv := &handlers.GRPCServer{request: requests.New()}
 	srv := handlers.New()
@@ -46,4 +58,51 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+}
+
+func RunGrapql(stopGrapql chan bool) {
+	go func() {
+		<-stopGrapql
+		os.Exit(1)
+	}()
+
+	port := "8086"
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: graph.New()}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	select {
+	case <-stopGrapql:
+		os.Exit(1)
+	}
+}
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	stopGrpc := make(chan bool, 1)
+	stopGrapql := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println(sig)
+		os.Remove("./csv_log.csv")
+		done <- true
+	}()
+
+	go func() {
+		<-done
+		stopGrpc <- true
+		stopGrapql <- true
+	}()
+
+	go RunGrpc(stopGrpc)
+	go RunGrapql(stopGrapql)
 }
--- gql/server.go
+++ gql/server.go
@@ -1,28 +1,28 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
+// import (
+// 	"log"
+// 	"net/http"
+// 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/IDL13/balance_ms/gql/graph"
-)
+// 	"github.com/99designs/gqlgen/graphql/handler"
+// 	"github.com/99designs/gqlgen/graphql/playground"
+// 	"github.com/IDL13/balance_ms/gql/graph"
+// )
 
-const defaultPort = "8080"
+// const defaultPort = "8086"
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+// func main() {
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = defaultPort
+// 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: graph.New()}))
+// 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: graph.New()}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+// 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+// 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
+// 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+// 	log.Fatal(http.ListenAndServe(":"+port, nil))
+// }
